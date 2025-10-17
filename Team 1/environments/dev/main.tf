8f97b4bdacc3f6eaa25f8440fbacac049966b963
blueoc-quan <quan.nguyen@blueoc.tech>
2025-10-17 10:58:05 +0700

    add cloud storage module

--- Team 1/environments/dev/main.tf
+++ Team 1/environments/dev/main.tf
@@ -1,7 +1,30 @@
 module "vpc" {
   source = "../../modules/vpc"
 
-  vpc_name     = "dev-vpc"
-  region       = var.region
-  subnet_cidr  = "10.0.0.0/24"
+  vpc_name    = "dev-vpc"
+  region      = var.region
+  subnet_cidr = "10.0.0.0/24"
+}
+
+module "app_bucket" {
+  source                      = "../../modules/storage"
+  bucket_name                 = "${var.project_name}-app-bucket-dev"
+  project_id                  = var.project_id
+  location                    = var.region
+  storage_class               = "STANDARD"
+  versioning_enabled          = true
+  force_destroy               = false
+  uniform_bucket_level_access = true
+
+  labels = {
+    env  = "dev"
+    team = "team-1"
+  }
+
+  iam_bindings = [
+    # {
+    #   role    = "roles/storage.objectViewer"
+    #   members = ["user:ducquank52t1@gmail.com"]
+    # }
+  ]
 }
